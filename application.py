df2daf41dab26c29c7e69f9a90ce99f3bba08e62
Kevin <kevin.choi@paust.io>
2019-05-29 16:07:59 +0900

    Add sqlalchemy

--- application.py
+++ application.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
     return "<h1>Hello decider</h1>"
+
 
 def create_app():
     return app
