12c9febfee3ee6fd8fe99c7375bccb57598650b5
Kevin <kevin.choi@paust.io>
2019-05-30 16:13:23 +0900

    Fix error in config.py mariadb password

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class MariaDBConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:decider@localhost/test?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:decider@@localhost/test?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
